7b0ec40d03b1b004ab9ea4994d3f7fa2ff77eb8b
codehunter-sk <93638366+codehunter-sk@users.noreply.github.com>
2022-03-01 19:48:34 +0530

    Add files via upload

---
+++ ATC/code_vrecog.py
@@ -0,0 +1,13 @@
+from PIL import Image
+from pytesseract import pytesseract
+def recog(img_path=0):
+    if not img_path:
+        img_path=input('Enter img_path for testing')
+    path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    img = Image.open(img_path)
+    pytesseract.tesseract_cmd = path_to_tesseract
+    text = pytesseract.image_to_string(img)
+    text=text.lower()
+    return text
+if __name__ == '__main__':
+    recog()
